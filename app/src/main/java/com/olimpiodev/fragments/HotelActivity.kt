bd3d85107e8d410d48f7cc23c70e88c357912398
Moisés Olimpio <moiza.olimpio@gmail.com>
2022-07-15 14:33:42 -0300

    Layout for tablets visualization

--- app/src/main/java/com/olimpiodev/fragments/HotelActivity.kt
+++ app/src/main/java/com/olimpiodev/fragments/HotelActivity.kt
@@ -2,6 +2,7 @@ package com.olimpiodev.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.olimpiodev.fragments.model.Hotel
 
 class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
@@ -11,10 +12,27 @@ class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListene
     }
 
     override fun onHotelClick(hotel: Hotel) {
-        showDetailsActivity(hotel.id)
+        if (isTablet()) {
+            showDetailsFragment(hotel.id)
+        }
+        if (isSmartphone()) {
+            showDetailsActivity(hotel.id)
+        }
     }
 
     private fun showDetailsActivity(hotelId: Long) {
         HotelDetailsActivity.open(this, hotelId)
+    }
+
+    private fun isTablet() = resources.getBoolean(R.bool.tablet)
+
+    private fun isSmartphone() = resources.getBoolean(R.bool.smartphone)
+
+    private fun showDetailsFragment(hotelId: Long) {
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
     }
 }
